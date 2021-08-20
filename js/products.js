cdd1480d2c7b48eb257e19e60f9a2dd5ee436501
joaquincal <jeliascal@gmail.com>
2021-08-20 15:17:37 -0300

    actualizacion productos 1

--- js/products.js
+++ js/products.js
@@ -12,3 +12,43 @@ document.addEventListener("DOMContentLoaded", function (e) {
     });
 });
 
+let url = "https://japdevdep.github.io/ecommerce-api/product/all.json"
+
+var getJSONData = function(url){
+    var result = {};
+    return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        return result;
+    });
+}
+
+let resultado = getJSONData(url).then(objeto => {
+    cargarDatos(objeto.data)
+})
+
+function cargarDatos(productos) {
+    let contenido = ``
+    for ( let producto of productos) {
+        contenido += `<p class="product__name">${producto.name}</p>
+        <p class="product__image">${producto.imgSrc}</p>
+        <p class="product__description">${producto.description}</p>
+        <p class="product__cost">${producto.currency}${producto.cost}</p>
+        <p class="product__soldcount">${producto.soldCount}</p>`
+    }
+    document.getElementById("products__list").innerHTML = contenido;
+} 
+
