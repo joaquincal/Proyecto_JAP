ef0c616916f6c2d9f7423454412070f452d3c76f
joaquincal <jeliascal@gmail.com>
2021-10-31 18:37:58 -0300

    arreglo botones de orden productos

--- js/my-profile.js
+++ js/my-profile.js
@@ -2,7 +2,7 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
-function loadImg() {
+function loadImg() { //funcion que carga la imagen del perfil
     let profilePic = document.getElementById('profilePhoto');
     let file = document.getElementById('photoInput').files[0];
 
@@ -19,7 +19,7 @@ function loadImg() {
 
 }
 
-function saveProfileData() {
+function saveProfileData() { //funcion que guarda los datos del usuario como elementos del objeto "profileData"
     let profileData = {};
     profileData.name = document.getElementById('profileName').value;
     profileData.lastname = document.getElementById('profileLastname').value;
@@ -28,14 +28,14 @@ function saveProfileData() {
     profileData.phone = document.getElementById('profilePhone').value;
     profileData.photo = document.getElementById('profilePhoto').src;
 
-    localStorage.setItem('profileInfo', JSON.stringify(profileData));
+    localStorage.setItem('profileInfo', JSON.stringify(profileData)); //el objeto profileData se guarda en local storage mediante el metodo JSON.stingify
 }
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    let profileData = JSON.parse(localStorage.getItem('profileInfo'));
+    let profileData = JSON.parse(localStorage.getItem('profileInfo')); //obtencion del objeto profileData mediante el llamado al local storage
 
-    if (profileData != null) {
+    if (profileData != null) { //Si la informacion del perfil no esta vacia, asigna los valores de las variables a los campos determinados del html
         document.getElementById('profileName').value = profileData.name;
         document.getElementById('profileLastname').value = profileData.lastname;
         document.getElementById('profileAge').value = profileData.age;
