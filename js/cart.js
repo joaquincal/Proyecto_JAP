e57c3141f8a64e2b622f2d44c27ad321a814a278
joaquincal <jeliascal@gmail.com>
2021-11-14 22:09:27 -0300

    entrega 7

--- js/cart.js
+++ js/cart.js
@@ -31,29 +31,79 @@ document.addEventListener("DOMContentLoaded", function (e) {
     }
 });
 
+function paymentDisplay(paymentType) {
+    let bankDisplay = document.getElementById('bank-info');
+    let cardDisplay = document.getElementById('card-info');
+    if (paymentType == 'bank') {
+        bankDisplay.classList.remove('invisible');
+        bankDisplay.classList.add('visible');
+        cardDisplay.classList.remove('visible');
+        cardDisplay.classList.add('invisible');
+        document.getElementById('bankName').required = true;
+        document.getElementById('bankAccount').required = true;
+
+        document.getElementById('cardName').required = false;
+        document.getElementById('cardNumber').required = false;
+        document.getElementById('card-date').required = false;
+        document.getElementById('cardCVV').required = false;
+
+        document.getElementById('cardName').value = '';
+        document.getElementById('cardNumber').value = '';
+        document.getElementById('card-date').value = '';
+        document.getElementById('cardCVV').value = '';
+    } else if (paymentType == 'card') {
+        cardDisplay.classList.remove('invisible');
+        cardDisplay.classList.add('visible');
+        bankDisplay.classList.remove('visible');
+        bankDisplay.classList.add('invisible');
+
+        document.getElementById('bankName').required = false;
+        document.getElementById('bankAccount').required = false;
+
+        document.getElementById('cardName').required = true;
+        document.getElementById('cardNumber').required = true;
+        document.getElementById('card-date').required = true;
+        document.getElementById('cardCVV').required = true;
+
+        document.getElementById('bankName').value = '';
+        document.getElementById('bankAccount').value = '';
+    }
+};
+
+function paymentClose() {
+    let bankDisplay = document.getElementById('bank-info');
+    let cardDisplay = document.getElementById('card-info');
+    bankDisplay.classList.remove('visible');
+    bankDisplay.classList.add('invisible');
+    cardDisplay.classList.remove('visible');
+    cardDisplay.classList.add('invisible')
+
+    document.getElementById('pay-card').checked = false;
+    document.getElementById('pay-bank').checked = false;
+}
+
 function shippingCost(value) {
     let subtotalArray = document.getElementsByClassName("subtotal"); //Obtengo la lista de elementos con clase "subtotal".
     let shipCost = 0;
+    let subtotalSum = 0;
     let percentage = parseInt(value) / 100;
     for (sub of subtotalArray) {
         shipCost += parseInt(sub.innerHTML) * percentage;
+        subtotalSum += parseFloat(sub.innerHTML);
     }
     document.getElementById('shippingPrice').innerHTML = shipCost;
-}
-
-function finalCartPrice(price) {
-    document.getElementById("finalProductsPrice").innerHTML = price;
+    document.getElementById('totalPrice').innerHTML = shipCost + subtotalSum;
 }
 
 function updatePrice(cost, count, id, currency) {
     if (currency == "UYU") {
         cost /= 40;
     }
-    document.getElementById(id).innerHTML = parseFloat(cost) * count; //Asigno el valor del costo multiplicado por la cantidad de articulos al elemento con id correspondiente.
-    let subtotalArray = document.getElementsByClassName("subtotal"); //Obtengo la lista de elementos con clase "subtotal".
+    document.getElementById(id).innerHTML = parseFloat(cost) * count;   //Asigno el valor del costo multiplicado por la cantidad de articulos al elemento con id correspondiente.
+    let subtotalArray = document.getElementsByClassName("subtotal");    //Obtengo la lista de elementos con clase "subtotal".
     let subtotal = 0;
     for (sub of subtotalArray) {
         subtotal += parseFloat(sub.innerHTML);
-    } //Recorro la lista de elementos con clase "subtotal" y sumo los precios individuales a una variable "subtotal".
-    finalCartPrice(subtotal) //llamo a la funcion finalCartPrice para mostrar el precio final de los articulos y el envio.
+    }                                                                   //Recorro la lista de elementos con clase "subtotal" y sumo los precios individuales a una variable "subtotal".
+    document.getElementById("finalProductsPrice").innerHTML = subtotal; //llamo a la funcion finalCartPrice para mostrar el precio final de los articulos y el envio.
 }
